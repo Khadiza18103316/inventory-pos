95009b89f12751af00e1b6d4ed8f9af651e00d1f
Khadiza <khadizaakhi756@gmail.com>
2022-04-27 12:08:06 +0600

    admin tamplate added

---
+++ resources/views/admin/partials/sidebar.blade.php
@@ -0,0 +1,12 @@
+<aside id="layout-menu" class="layout-menu menu-vertical menu bg-menu-theme">
+
+    <ul class="menu-inner py-1">
+      <!-- Dashboard -->
+      <li class="menu-item active">
+        <a href="#" class="menu-link">
+          <i class="menu-icon tf-icons bx bx-home-circle"></i>
+          <div data-i18n="Analytics">Dashboard</div>
+        </a>
+      </li>
+    </ul>
+  </aside>
