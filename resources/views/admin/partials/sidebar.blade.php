8491328ec38b4aba6dda4427f0d0635a804e4b53
Khadiza <khadizaakhi756@gmail.com>
2022-04-30 12:10:10 +0600

    employee and supplier model,migration created

--- resources/views/admin/partials/sidebar.blade.php
+++ resources/views/admin/partials/sidebar.blade.php
@@ -8,5 +8,12 @@
           <div data-i18n="Analytics">Dashboard</div>
         </a>
       </li>
+
+      <li class="menu-item">
+        <a href="cards-basic.html" class="menu-link">
+          <i class="menu-icon tf-icons bx bx-collection"></i>
+          <div data-i18n="Basic">Cards</div>
+        </a>
+      </li>
     </ul>
   </aside>
