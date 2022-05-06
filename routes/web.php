3f78d71e1a54e1765b1e6db40e7fcf03746dd978
Khadiza <khadizaakhi756@gmail.com>
2022-05-06 20:37:01 +0600

    route group updated

--- routes/web.php
+++ routes/web.php
@@ -9,24 +9,26 @@ use Illuminate\Support\Facades\Route;
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Route::group(['prefix' => '/'], function () {
+// Admin Start
+Route::group(['prefix'=>'/'],function(){
+Route::get('/', function () {
+return view('admin.pages.dashboard.dash');
+})->name('admin.dashboard');
 
 //  Dashboard
-    Route::get('/dashboard', [DashboardController::class, 'dashboard'])->name('admin.dashboard');
-});
+Route::get('/dashboard', [DashboardController::class, 'dashboard'])->name('admin.dashboard');
 
 // Employee
-    Route::get('/employee/index', [EmployeeController::class, 'index'])->name('employee.index');
-    Route::get('/employee/create', [EmployeeController::class, 'create'])->name('employee.create');
-    Route::post('/employee/store', [EmployeeController::class, 'store'])->name('employee.store');
-    Route::get('/employee/edit/{id}', [EmployeeController::class, 'edit'])->name('employee.edit');
-    Route::put('/employee/update/{id}', [EmployeeController::class, 'update'])->name('employee.update');
-    Route::get('/employee/delete/{id}', [EmployeeController::class, 'delete'])->name('employee.delete');
+Route::get('/employee/index', [EmployeeController::class, 'index'])->name('employee.index');
+Route::get('/employee/create', [EmployeeController::class, 'create'])->name('employee.create');
+Route::post('/employee/store', [EmployeeController::class, 'store'])->name('employee.store');
+Route::get('/employee/edit/{id}', [EmployeeController::class, 'edit'])->name('employee.edit');
+Route::put('/employee/update/{id}', [EmployeeController::class, 'update'])->name('employee.update');
+Route::get('/employee/delete/{id}', [EmployeeController::class, 'delete'])->name('employee.delete');
 
 
 // Supplier
-    Route::get('/supplier/index', [SupplierController::class, 'index'])->name('supplier.index');
-    Route::get('/supplier/create', [SupplierController::class, 'create'])->name('supplier.create');
-    Route::post('/supplier/store', [SupplierController::class, 'store'])->name('supplier.store');
+Route::get('/supplier/index', [SupplierController::class, 'index'])->name('supplier.index');
+Route::get('/supplier/create', [SupplierController::class, 'create'])->name('supplier.create');
+Route::post('/supplier/store', [SupplierController::class, 'store'])->name('supplier.store');
+});
